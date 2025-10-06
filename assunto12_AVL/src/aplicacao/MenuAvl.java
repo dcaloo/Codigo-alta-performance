6dc93aa17ba728863409de5b8363a838c8ebd5e5
Eduardo Caló <167481143+dcaloo@users.noreply.github.com>
2025-10-06 11:39:28 -0300

    Final Commit assunto 12

--- assunto12_AVL/src/aplicacao/MenuAvl.java
+++ assunto12_AVL/src/aplicacao/MenuAvl.java
@@ -13,22 +13,41 @@ public class MenuAvl {
             System.out.println("0 - Sair");
             System.out.println("1 - Inserir");
             System.out.println("2 - Mostra FB de cada nó");
+            System.out.println("3 - Remove nó escolhido por seu conteudo");
             opcao = sc.nextInt();
             switch (opcao) {
                 case 0:
                     System.out.println("Encerrado");
                 break;
                 case 1:
-                    System.out.println("Informe o valor a ser inserido");
+                    System.out.println("Informe o valor a ser inserido: ");
                     int valor = sc.nextInt();
                     avl.root = avl.inserirAVL(avl.root, valor);
                     avl.atualizaAlturas(avl.root);
                 break;
                 case 2:
-                    System.out.println("Apresentação da AVL");
+                    System.out.println("Apresentação da AVL: ");
                     avl.mostraFB(avl.root);
                 break;
-                default:
+                case 3:
+                    System.out.println("Digite o valor a ser removido: ");
+                    valor = sc.nextInt();
+                    avl.root = avl.removeValor(avl.root, valor);
+                    avl.root = avl.atualizaAlturaBalanceamento(avl.root);
+                    avl.atualizaAlturas(avl.root);
+                    break;
+                case 4:
+                    System.out.println("Quantidade de nós na AVL: " + avl.contaNos(avl.root, 0));
+                    break;
+                case 5:
+                    System.out.println("Informe o valor a ser pesquisado: ");
+                    valor = sc.nextInt();
+                    if (avl.consulta(avl.root, valor))
+                        System.out.println("Valor encontrado na AVL");
+                    else
+                        System.out.println("Valor não encontrado na AVL");
+                    break;
+                    default:
                     System.out.println("Opção Invalida");
             }
 
--- assunto12_AVL/src/arvores/AVLint.java
+++ assunto12_AVL/src/arvores/AVLint.java
@@ -111,6 +111,83 @@ public class AVLint {
         }
     }
 
+    public NO atualizaAlturaBalanceamento (NO p) {
+        /*atualiza informação da altura de cada nó depois da remoção percorre a árvore usando percurso pós-ordem para ajustar primeiro os nós folhas (profundidade maior) e depois os níveis acima */
+        if( p != null) {
+            p.esq = atualizaAlturaBalanceamento (p.esq);
+            if (p.esq == null)
+                p.hEsq = 0;
+            else  if (p.esq.hEsq > p.esq.hDir)
+                p.hEsq = p.esq.hEsq+1;
+            else
+                p.hEsq = p.esq.hDir+1;
+            p.dir = atualizaAlturaBalanceamento (p.dir);
+            if (p.dir == null)
+                p.hDir = 0;
+            else if (p.dir.hEsq > p.dir.hDir)
+                p.hDir = p.dir.hEsq+1;
+            else
+                p.hDir = p.dir.hDir+1;
+            p = balanceamento(p);
+        }
+        return p;
+    }
+
+
+    public NO removeValor(NO p, int info) {
+        if (p != null) {
+            if (info == p.dado) {
+                if (p.esq == null && p.dir == null)
+                    return null;
+                else {
+                    if (p.esq == null)
+                        return p.dir;
+                    else if (p.dir == null)
+                        return p.esq;
+                    else {
+                        NO aux, ref;
+                        ref = p.dir;
+                        aux = p.dir;
+                        while (aux.esq != null)
+                            aux = aux.esq;
+                        aux.esq = p.esq;
+                        return ref;
+                    }
+                }
+            } else { // procura dado a ser removido na ABB
+                if (info < p.dado)
+                    p.esq = removeValor(p.esq, info);
+                else
+                    p.dir = removeValor(p.dir, info);
+            }
+        }
+        return p;
+    }
+
+    public int contaNos(NO p, int cont) {
+        if (p != null) {
+            cont++;
+            cont = contaNos(p.esq, cont);
+            cont = contaNos(p.dir, cont);
+        }
+        return cont;
+    }
+
+    public boolean consulta(NO p, int valor) {
+        if(p != null) {
+            if(valor == p.dado) {
+                return true;
+            }
+            else if (valor < p.dado){
+                return consulta(p.esq, valor);
+            } else {
+                return consulta(p.dir, valor);
+            }
+        } else {
+            return false;
+        }
+    }
+
 
 
 
